10ae482d6e3c750471c736999f88d5d46fcf62a5
Vimin2611 <krish8102@gmail.com>
2025-02-27 18:11:08 +0000

    Add files via upload

---
+++ a.py
@@ -0,0 +1,6 @@
+# Name 
+print("Hi i am Rosie Posy, very cosy in my homsy")
+# Date of birth
+print("My date of birth is on the 20th of may")
+# Number
+print("The sum of my favourite numbers 6 and 26 is", 6 + 26 ) 
